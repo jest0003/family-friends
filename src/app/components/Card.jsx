962a8a222526306bbeded81ba472d47f9cb09d5b
Jerie <jerie.stolbergrohr@gmail.com>
2026-04-13 20:45:43 +0200

    fav virker nu!

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -3,16 +3,18 @@ import { FaRegStar } from "react-icons/fa";
 
 const Card = () => {
     return ( <section className="rounded-2xl bg-white">
-        <FaRegStar 
-        color="pink"
-        className="icon anchored"/>
+        <div className="relative">
+<FaRegStar 
+        color="white"
+        className="icon absolute right-1 top-1 bg-[rgba(254,254,254,0.25)] rounded-full p-0.5"/>
 <Image
                 src="https://placecats.com/g/299/299"
-                alt="Picture of the author"
+                alt="Picture of the doggo"
                 width={160}
                 height={100}
-                className="star-anchor rounded-xl"
+                className="rounded-xl"
               />
+        </div>
               <div className="p-2">
                 <h2 className="font-bold">Doggo breed</h2>
               <p className="w-24 text-stone-400">Originate, noget længere</p>
