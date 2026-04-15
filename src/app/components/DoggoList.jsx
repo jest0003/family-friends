b80422a9b8badec44e7fd5e9f363487bedb32901
Jerie <jerie.stolbergrohr@gmail.com>
2026-04-15 10:05:53 +0200

    zustand

--- src/app/components/DoggoList.jsx
+++ src/app/components/DoggoList.jsx
@@ -11,31 +11,35 @@ const DoggoList = () => {
 
 const FetchBreed = async () => {
   "use server";
-  const response = await fetch(
-    "https://api.thedogapi.com/v1/breeds",
-    {
-      headers: {
-        "x-api-key": process.env.API_KEY,
+  try {
+    const response = await fetch(
+      "https://api.thedogapi.com/v1/breeds",
+      {
+        headers: {
+          "x-api-key": process.env.API_KEY,
+        },
       },
-    },
-  );
-  const breeds = await response.json();
-
-  return breeds.map((breed) => {
-    return (
-      <Link
-        key={breed.id}
-        href={`/detailview/${breed.id}`}
-      >
-        <Card
-          key={breed.id}
-          breedGroup={breed.breed_group}
-          origin={breed.origin}
-          doggoPicture={breed.image.url}
-        />
-      </Link>
     );
-  });
+    const breeds = await response.json();
+
+    return breeds.map((breed) => {
+      return (
+        <Link
+          key={breed.id}
+          href={`/detailview/${breed.id}`}
+        >
+          <Card
+            key={breed.id}
+            breedGroup={breed.breed_group}
+            origin={breed.origin}
+            doggoPicture={breed.image.url}
+          />
+        </Link>
+      );
+    });
+  } catch (error) {
+    return <p>Failed to load doggos.</p>;
+  }
 };
 
 export default DoggoList;
